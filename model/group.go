c78cf944a103bca7c536a4d1deafb1bebd7c985f
gramizor <granin17@yandex.ru>
2023-12-25 10:39:01 +0300

    updated models

---
+++ model/group.go
@@ -0,0 +1,18 @@
+// Package model ...
+package model
+
+// Group представляет информацию о группе.
+type Group struct {
+	GroupID     uint   `gorm:"type:serial;primarykey" json:"group_id"`
+	GroupCode   string `json:"group_code" example:"RT5-51B"`
+	Contacts    string `json:"contacts" example:"+7(999)999-99-99"`
+	Course      int    `json:"course" example:"1"`
+	Students    int    `json:"students" example:"23"`
+	GroupStatus string `json:"group_status" example:"обучается"`
+	Photo       string `json:"photo" example:"http://example.com/group.jpg"`
+}
+
+type GroupsGetResponse struct {
+	Groups     []Group `json:"groups"`
+	FeedbackID uint    `json:"feedback_id" example:"1"`
+}
